1fd451d9b59eb2e54bc0c212b82ed405ec1c158b
Dan Yeaw <dan@yeaw.me>
2022-08-28 15:49:20 -0400

    Add initial front end to filter by type

--- ispythepokemon/app.py
+++ ispythepokemon/app.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlmodel import Session, or_, select
 
 from ispythepokemon.database import create_db_and_tables, engine
 from ispythepokemon.models import Pokemon
 
 app = FastAPI()
+templates = Jinja2Templates(directory="ispythepokemon/templates")
 
 
 def get_session():
@@ -17,14 +20,16 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.get("/pokemon/")
-def read_pokemon():
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
     with Session(engine) as session:
-        return session.exec(select(Pokemon)).all()
+        pokemon = all_pokemon()
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "pokemon": pokemon}
+        )
 
 
-@app.get("/type/{pokemon_types}")
-def read_pokemon_by_type(pokemon_types: str):
+def get_pokemon_by_type(pokemon_types: str):
     with Session(engine) as session:
         if " " in pokemon_types:
             types_list = pokemon_types.split()
@@ -36,3 +41,17 @@ def read_pokemon_by_type(pokemon_types: str):
 
         results = session.exec(statement)
         return results.all()
+
+
+@app.get("/pokemon/")
+def all_pokemon():
+    with Session(engine) as session:
+        return session.exec(select(Pokemon)).all()
+
+
+@app.get("/type/")
+def read_pokemon_by_type(request: Request, pokemon_types: str):
+    pokemon = get_pokemon_by_type(pokemon_types)
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "pokemon": pokemon}
+    )
