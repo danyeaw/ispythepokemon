e0934d77135e135fe69054ea4a95e9893977d5fc
Dan Yeaw <dan@yeaw.me>
2022-08-31 20:22:07 -0400

    Simplify routes

--- ispythepokemon/app.py
+++ ispythepokemon/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -10,6 +10,19 @@ from ispythepokemon.models import Pokemon
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="ispythepokemon/static"), name="static")
 templates = Jinja2Templates(directory="ispythepokemon/templates")
+
+
+def get_types():
+    with Session(engine) as session:
+        statement = select(Pokemon.type).distinct()
+        distinct_types = session.exec(statement).all()
+        single_types = set()
+        for type in distinct_types:
+            single_types.update(type.split())
+        return single_types
+
+
+types = sorted(get_types())
 
 
 def get_session():
@@ -24,14 +37,13 @@ def on_startup():
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    pokemon = []
-    types = all_types()
     return templates.TemplateResponse(
-        "index.html", {"request": request, "pokemon": pokemon, "types": types}
+        "index.html", {"request": request, "pokemon": [], "types": types}
     )
 
 
-def get_pokemon_by_type(type1: str, type2: str | None):
+@app.get("/type/")
+def read_pokemon_by_type(request: Request, type1: str, type2: str | None = None):
     with Session(engine) as session:
         if type2:
             statement = select(Pokemon).where(
@@ -41,29 +53,7 @@ def get_pokemon_by_type(type1: str, type2: str | None):
             statement = select(Pokemon).where(Pokemon.type == type1)
 
         results = session.exec(statement)
-        return results.all()
-
-
-@app.get("/pokemon/")
-def all_pokemon():
-    with Session(engine) as session:
-        return session.exec(select(Pokemon)).all()
-
-
-def all_types():
-    with Session(engine) as session:
-        statement = select(Pokemon.type).distinct()
-        distinct_types = session.exec(statement).all()
-        single_types = set()
-        for type in distinct_types:
-            single_types.update(type.split())
-        return single_types
-
-
-@app.get("/type/")
-def read_pokemon_by_type(request: Request, type1: str, type2: str | None = None):
-    pokemon = get_pokemon_by_type(type1, type2)
-    types = all_types()
+        pokemon = results.all()
     return templates.TemplateResponse(
         "index.html", {"request": request, "pokemon": pokemon, "types": types, "type1": type1, "type2": type2}
     )
